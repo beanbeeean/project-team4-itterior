77d85a0649829825dff5a014eeea3c6d666c9417
beanbeeean <beanbeeean@naver.com>
2023-08-29 11:45:55 +0900

    [23.08.29] 유저 새 비밀번호 발급 서비스 완성

--- src/main/java/com/office/house/user/IUserService.java
+++ src/main/java/com/office/house/user/IUserService.java
@@ -7,5 +7,6 @@ public interface IUserService {
 	public Map<String, Object> createAccountConfirm(Map<String, String> msgMap);
 
 	public Map<String, Object> userLoginConfirm(Map<String, String> msgMap);
-	
+
+    public Map<String, Object> findPasswordConfirm(Map<String, String> msgMap);
 }
